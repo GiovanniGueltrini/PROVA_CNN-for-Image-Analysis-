3412f91312b91ae2af572ace295d49dbebabbdcd
GiovanniGueltrini <163996552+GiovanniGueltrini@users.noreply.github.com>
2025-08-31 09:57:32 +0200

    Update __init__.py

--- function/__init__.py
+++ function/__init__.py
@@ -2,4 +2,4 @@ from .grad import run_gradcam_with,
 from .segmentation import Dice_Sorensen, segmentation_prova
 from .dm_image.py import create_dummy_images, make_the_object, generate_dataset
 
-__all__ = ["run_gradcam_with", "Dice_Sorensen", "segmentation_prova"]
+__all__ = ["run_gradcam_with", "Dice_Sorensen", "segmentation_prova", "create_dummy_images", "make_the_object", "generate_dataset"]
