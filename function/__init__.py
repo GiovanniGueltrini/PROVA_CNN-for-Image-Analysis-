4217579ebf26d71174cfdd47c5366b38cc4db80a
GiovanniGueltrini <163996552+GiovanniGueltrini@users.noreply.github.com>
2025-08-31 17:45:01 +0200

    Update __init__.py

--- function/__init__.py
+++ function/__init__.py
@@ -1,6 +1,6 @@
 from .grad import run_gradcam_with
 from .segmentation import Dice_Sorensen, segmentation_prova
 from .dm_image.py import create_dummy_images, make_the_object, generate_dataset
-from .costum_model_Resnet.py import CustomResNet
+from .costum_model_Resnet import CustomResNet
 
 __all__ = ["run_gradcam_with", "Dice_Sorensen", "segmentation_prova", "create_dummy_images", "make_the_object", "generate_dataset","CustomResNet"]
